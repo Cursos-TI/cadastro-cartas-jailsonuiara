535afc1f6e7f8dccc40d93ec543b6eed0f7161fa
jailsonuiara <jailsonuiara@gmail.com>
2026-03-23 17:16:19 -0300

    coloquei printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+   printf ("ola mundo")
 
   // Área para exibição dos dados da cidade
 
